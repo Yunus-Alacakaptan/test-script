65f70b5077692cede1bfc917c05cce3bacac7811
Yunus Alacakaptan <yunus.alacakaptan@alan.co>
2024-12-11 15:43:18 +0300

    Added utils and resources

---
+++ src/main/java/utils/UrlConstants.java
@@ -0,0 +1,6 @@
+package utils;
+
+public class UrlConstants {
+    public static final String BASE_URL = "https://app-staging.qlub.cloud/";
+    public static final String PAYMENT_PAGE = "qr/ae/qz-dummy-ch/3/_/_/a1415a10d6";
+}
