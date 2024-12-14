1a4ff1c94a8729ea3d634427684aedff94fe2443
Yunus Alacakaptan <yunus.alacakaptan@alan.co>
2024-12-14 20:25:15 +0300

    Updated driver specifications

--- src/main/java/driver/Driver.java
+++ src/main/java/driver/Driver.java
@@ -1,20 +1,10 @@
 package driver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.InvalidArgumentException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.time.Duration;
-import java.util.Properties;
-import java.util.concurrent.TimeUnit;
 
 public class Driver {
 
@@ -23,8 +13,6 @@ public class Driver {
     private static final String BROWSER_CONFIG_FILE = "browser.properties";
     private static final int PAGE_TIME_OUT = 30;
     private static WebDriver driver;
-
-    private static final Logger log = LoggerFactory.getLogger(Driver.class);
 
     private Driver() {
     }
